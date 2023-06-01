f8f09b8bfee0988ed5d867ba2b001318321fddcf
unknown <yangzhikang@company.local>
2023-06-01 12:58:34 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Learning_STM32
+
+第一节课：点亮LED灯
+
+Step1.开启GPIO端口的时钟
+Step2.控制GPIO口的方向
+Step3.配置GPIO的数据输出寄存器
--- register_mapping/main.c
+++ register_mapping/main.c
@@ -1,7 +1,14 @@
 #include "stm32f4xx.h"
-
 int main(void)
 {
+	/* xxx  &= ~((0x03) << (2 * 6))    目标位清0，其他位保持不变  */
+	
+	/* Output High Level*/
+	*(unsigned int *)(0x40000000 + 0x14) |= (1<<6);
+	
+	/* Output Low Level*/
+	*(unsigned int *)(0x40000000 + 0x14) &= ~(1<<6);
+	
 	while(1)
 	{
 		/**/
